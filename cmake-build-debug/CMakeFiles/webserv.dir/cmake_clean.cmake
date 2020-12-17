3a8d95381016864bb7c5f09b8ef9c37a4128ae21
brokenfiles <louis.nevysup@gmail.com>
2020-12-17 14:52:39 +0100

    Adding default structure

---
+++ cmake-build-debug/CMakeFiles/webserv.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserv.dir/srcs/webserv.cpp.obj"
+  "libwebserv.dll.a"
+  "webserv.exe"
+  "webserv.exe.manifest"
+  "webserv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
