efa01f162aa62e3acfabb272c686760d3700196d
user42 <m4xth9r@gmail.com>
2020-12-17 14:51:51 +0100

    bases

---
+++ cmake-build-debug/CMakeFiles/webserv.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user42/Bureau/projects/webserv/client.cpp" "/home/user42/Bureau/projects/webserv/cmake-build-debug/CMakeFiles/webserv.dir/client.cpp.o"
+  "/home/user42/Bureau/projects/webserv/server.cpp" "/home/user42/Bureau/projects/webserv/cmake-build-debug/CMakeFiles/webserv.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/webserv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserv.dir/client.cpp.o"
+  "CMakeFiles/webserv.dir/server.cpp.o"
+  "webserv"
+  "webserv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
